e6e3ee01e7864951b79d5ef4ac6ff26dd0e6540f
reece-vlr <reece.vlr@gmail.com>
2023-07-06 18:20:24 +0800

    Add calculator functionality (basics)

---
+++ index.js
@@ -0,0 +1,119 @@
+/* Init Variables */
+let x;      // Num 1
+let y;      // Num 2
+let op;     // Operator
+let yFlag = false;
+let opFlag = false;
+let ansFlag = false;
+
+const numbers = document.querySelectorAll('.num-btn');
+const operations = document.querySelectorAll('.op-btn');
+const clrBtn = document.querySelector('#clr');
+const ansBtn = document.querySelector('#ans');
+const inDisplay = document.querySelector('#in-display');
+const opDisplay = document.querySelector('#op-display');
+
+/* Event Listeners */
+numbers.forEach(num => {
+    num.addEventListener('click', setDisplay);
+});
+
+operations.forEach(operator => {
+    operator.addEventListener('click', function(e) {
+        x = Number(inDisplay.textContent);
+        op = e.target.textContent;
+        opFlag = true;
+
+        if (ansFlag) {
+            opDisplay.textContent = '';
+        }
+
+        let text = document.createTextNode(`${x} ${e.target.textContent}`);
+        opDisplay.appendChild(text);
+    });
+});
+
+ansBtn.addEventListener('click', function(e) {
+    if (yFlag) {
+        y = Number(inDisplay.textContent);
+        setDisplay(e);
+    }
+});
+
+clrBtn.addEventListener('click', clearDisplay);
+
+/* Calculator Functions */
+function setDisplay(e) {
+    if (inDisplay.textContent === '0') {
+        inDisplay.textContent = '';
+    }
+
+    if (ansFlag) {
+        inDisplay.textContent = '';
+        ansFlag = false;
+    }
+
+    if (opFlag) {
+        inDisplay.textContent = '';
+        opFlag = false;
+        yFlag = true;
+    }
+
+    if (e.target.textContent === '=') {
+        inDisplay.textContent = '';
+        yFlag = false;
+
+        let opText = document.createTextNode(` ${y} = `);
+        opDisplay.appendChild(opText);
+
+        x = Number(operate(op, x, y));
+        let inText = document.createTextNode(x);
+        inDisplay.appendChild(inText);
+
+        ansFlag = true;
+        return;
+    }
+
+    let text = document.createTextNode(e.target.textContent);
+    inDisplay.appendChild(text);
+}
+
+function clearDisplay() {
+    inDisplay.textContent = '0';
+    opDisplay.textContent = '';
+
+    yFlag = false;
+    opFlag = false;
+}
+
+/* Math Functions */
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(op, x, y) {
+    if (op === '+') {
+        return add(x, y);
+    }
+    else if (op === '-') {
+        return subtract(x, y);
+    }
+    else if (op === '*') {
+        return multiply(x, y);
+    }
+    else if (op === '/') {
+        return divide(x, y);
+    }
+}
