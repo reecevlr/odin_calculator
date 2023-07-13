8eb8b2eaa5eb477d6ea46ee02252a502c515e21e
reece-vlr <reece.vlr@gmail.com>
2023-07-13 21:08:22 +0800

    Add keyboard support

--- index.js
+++ index.js
@@ -16,24 +16,95 @@ const inDisplay = document.querySelector('#in-display');
 const opDisplay = document.querySelector('#op-display');
 
 /* Event Listeners */
+window.addEventListener('keydown', function(e) {
+    switch (e.key) {
+        case 'Backspace':
+            clearEntry();
+            break;
+        case '+':
+            supportKey('+');
+            break;
+        case '-':
+            supportKey('-');
+            break;
+        case '*':
+            supportKey('*');
+            break;
+        case '/':
+            e.preventDefault();
+            supportKey('/');
+            break;
+        case 'Enter':
+            supportKey('ans');
+            break;
+        case '1':
+            supportKey('1');
+            break;
+        case '2':
+            supportKey('2');
+            break;
+        case '3':
+            supportKey('3');
+            break;
+        case '4':
+            supportKey('4');
+            break;
+        case '5':
+            supportKey('5');
+            break;
+        case '6':
+            supportKey('6');
+            break;
+        case '7':
+            supportKey('7');
+            break;
+        case '8':
+            supportKey('8');
+            break;
+        case '9':
+            supportKey('9');
+            break;
+        case '0':
+            supportKey('0');
+            break;
+    }
+});
+
+function supportKey(key) {
+    prepareDisplayOutput();
+
+    switch (key) {
+        case '+':
+            setValues('\u002B');
+            return;
+        case '-':
+            setValues('\u002D');
+            return;
+        case '*':
+            setValues('\u00D7');
+            return;
+        case '/':
+            setValues('\u00F7');
+            return;
+        case 'ans':
+            if (yFlag) {
+                y = Number(inDisplay.textContent);
+            }
+            prepareDisplayOutput();
+            setAnswer();
+            return;
+    };
+
+    let text = document.createTextNode(key);
+    inDisplay.appendChild(text);
+}
+
 numbers.forEach(num => {
     num.addEventListener('click', setDisplay);
 });
 
 operations.forEach(operator => {
-    operator.addEventListener('click', function(e) {
-        x = Number(inDisplay.textContent);
-        op = e.target.textContent;
-        opFlag = true;
-
-        if (ansFlag) {
-            opDisplay.textContent = '';
-        }
-
-        opDisplay.textContent = '';
-        let text = document.createTextNode(`${x} ${e.target.textContent}`);
-        opDisplay.appendChild(text);
-    });
+    operator.addEventListener('click', setOperator)
 });
 
 decBtn.addEventListener('click', function(e) {
@@ -45,7 +116,19 @@ decBtn.addEventListener('click', function(e) {
     }
 });
 
-ceBtn.addEventListener('click', function(e) {
+ansBtn.addEventListener('click', function(e) {
+    if (yFlag) {
+        y = Number(inDisplay.textContent);
+        setDisplay(e);
+    }
+});
+
+ceBtn.addEventListener('click', clearEntry);
+
+clrBtn.addEventListener('click', clearDisplay);
+
+/* Calculator Functions */
+function clearEntry() {
     if (inDisplay.textContent === '0') {
         return;
     }
@@ -57,27 +140,17 @@ ceBtn.addEventListener('click', function(e) {
         inDisplay.textContent = '0';
         return;
     }
-});
+}
 
-ansBtn.addEventListener('click', function(e) {
-    if (yFlag) {
-        y = Number(inDisplay.textContent);
-        setDisplay(e);
-    }
-});
-
-clrBtn.addEventListener('click', clearDisplay);
-
-/* Calculator Functions */
-function setDisplay(e) {
+function prepareDisplayOutput() {
     if (inDisplay.textContent === '0') {
         inDisplay.textContent = '';
     }
 
-    if (ansFlag) {
-        inDisplay.textContent = '';
-        ansFlag = false;
-    }
+    // if (ansFlag) {
+    //     inDisplay.textContent = '';
+    //     ansFlag = false;
+    // }
 
     if (opFlag) {
         inDisplay.textContent = '';
@@ -85,23 +158,51 @@ function setDisplay(e) {
         yFlag = true;
     }
 
+    return;
+}
+
+function setAnswer() {
+    inDisplay.textContent = '';
+    yFlag = false;
+
+    let opText = document.createTextNode(` ${y} = `);
+    opDisplay.appendChild(opText);
+
+    x = Number(operate(op, x, y));
+    let inText = document.createTextNode(x);
+    inDisplay.appendChild(inText);
+
+    ansFlag = true;
+}
+
+function setDisplay(e) {
+    prepareDisplayOutput();
+
     if (e.target.textContent === '=') {
-        inDisplay.textContent = '';
-        yFlag = false;
-
-        let opText = document.createTextNode(` ${y} = `);
-        opDisplay.appendChild(opText);
-
-        x = Number(operate(op, x, y));
-        let inText = document.createTextNode(x);
-        inDisplay.appendChild(inText);
-
-        ansFlag = true;
+        setAnswer();
         return;
     }
 
     let text = document.createTextNode(e.target.textContent);
     inDisplay.appendChild(text);
+}
+
+function setOperator(e) {
+    setValues(e.target.textContent);
+}
+
+function setValues(operator) {
+    x = Number(inDisplay.textContent);
+    op = operator;
+    opFlag = true;
+
+    // if (ansFlag) {
+    //     opDisplay.textContent = '';
+    // }
+
+    opDisplay.textContent = '';
+    let text = document.createTextNode(`${x} ${op}`);
+    opDisplay.appendChild(text);
 }
 
 function clearDisplay() {
